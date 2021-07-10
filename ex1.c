5965242af6b9f7c20b81632975ed5967e81a0b6c
Guillem <Guillem@DESKTOP-803DQIG>
2021-07-10 15:26:18 +0200

    Project creation

---
+++ ex1.c
@@ -0,0 +1,8 @@
+/*
+ * ex1.c
+ *
+ *  Created on: 10 jul 2021
+ *      Author: Guillem
+ */
+
+
---
+++ ex1.h
@@ -0,0 +1,13 @@
+/*
+ * ex1.h
+ *
+ *  Created on: 10 jul 2021
+ *      Author: Guillem
+ */
+
+#ifndef EX1_H_
+#define EX1_H_
+
+
+
+#endif /* EX1_H_ */
