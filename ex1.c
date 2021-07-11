5f0b6a56a0edd40eceab12cba35e8aa288413c71
Guillem <Guillem@DESKTOP-803DQIG>
2021-07-11 21:06:40 +0200

    RobotTask interruption problem detected in case sensor detects an object (comment)

--- ex1.c
+++ ex1.c
@@ -20,17 +20,23 @@ int main (){
 
 	do {
 		/* Read EEPROM task command */
-		SPIEepromEnable();
-		eeprom_command = SPIRead();
-		SPIEepromDisable();
+		SPIEepromEnable(); //1 us
+		eeprom_command = SPIRead(); //5 us
+		SPIEepromDisable(); //1 us
 
 		/* Execution of the task*/
-		RobotTask (eeprom_command);
+		RobotTask (eeprom_command); //100 ms
+
+		/*
+		 * The main problem I see here is how can I interrupt the
+		 * RobotTask in case the sensor detect an object, as it is
+		 * the slowest function
+		 */
 
 		/* Condition to stop the main */
 		if (sensor_value == SENSOR_LIMIT)
 		{
-			RobotStop();
+			RobotStop(); //15us
 			stop_program = 1;
 		}
 
@@ -41,9 +47,9 @@ int main (){
 
 void TimerISR (void){
 	/* Read sensor value */
-	SPISensorEnable();
-	sensor_value = SPIRead();
-	SPISensorDisable();
+	SPISensorEnable(); //1 us
+	sensor_value = SPIRead(); //5 us
+	SPISensorDisable(); // 1 us
 
 	TimerInit();
 }
