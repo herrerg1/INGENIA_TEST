e147a2d220b7b775d388537a30617f3d783ea6e7
Guillem <Guillem@DESKTOP-803DQIG>
2021-07-11 20:28:53 +0200

    Initialization and creation of continuous loop

--- ex1.c
+++ ex1.c
@@ -4,5 +4,20 @@
  *  Created on: 10 jul 2021
  *      Author: Guillem
  */
+#include <stdint.h>
+#include <stdio.h>
+
+int main (){
+
+	/* Initialization */
+	TimerInit();
+	SPInit();
 
 
+	while (1){
+
+
+	}
+
+	return 0;
+}
